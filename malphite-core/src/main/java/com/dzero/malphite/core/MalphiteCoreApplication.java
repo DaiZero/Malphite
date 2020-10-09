7e341fd66f218abe16bb6665fa41d8c02d2ea589
DZero <dzd624175217@gmail.com>
2020-10-09 17:28:18 +0800

    add module : malphite-core

---
+++ malphite-core/src/main/java/com/dzero/malphite/core/MalphiteCoreApplication.java
@@ -0,0 +1,13 @@
+package com.dzero.malphite.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalphiteCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalphiteCoreApplication.class, args);
+	}
+
+}
---
+++ malphite-core/src/test/java/com/dzero/malphite/core/MalphiteCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dzero.malphite.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalphiteCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
